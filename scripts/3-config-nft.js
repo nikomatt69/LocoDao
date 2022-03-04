39ed0664010cd4775391773b29cce92e9e9c60ac
Nikomatt.eth <94803063+nikomatt69@users.noreply.github.com>
2022-03-04 15:28:15 +0000

    Initial commit

---
+++ scripts/3-config-nft.js
@@ -0,0 +1,21 @@
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x787a7533994CF01F041258425435B82ac2f5321c",
+);
+
+(async () => {
+  try {
+    await bundleDrop.createBatch([
+      {
+        name: "LocoDAO Member Card",
+        description: "This NFT will give you access to LocoDAO!",
+        image: readFileSync(""),
+      },
+    ]);
+    console.log("✅ Successfully created a new NFT in the drop!");
+  } catch (error) {
+    console.error("failed to create the new NFT", error);
+  }
+})()
